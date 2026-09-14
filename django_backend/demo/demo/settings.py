d9405327a1b247e09746627a17893f996f5303d8
Rainhard07 <rainhard07@users.noreply.github.com>
2026-09-14 16:16:32 +0000

    host config

--- django_backend/demo/demo/settings.py
+++ django_backend/demo/demo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-z663x+k6*vek&ws41$*-qbu5n$4b^i*+r@4sfah4yl5%$*szcn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000', 'http://localhost:8000']
 # Application definition
